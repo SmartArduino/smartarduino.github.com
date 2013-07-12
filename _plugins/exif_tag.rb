bcf3fbf00a437e6225bff4beb633c08aee3ec816
realasking <realasking@mdbbs.org>
2013-07-12 19:35:56 +0800

    测试贴图4

--- _plugins/exif_tag.rb
+++ _plugins/exif_tag.rb
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 # A Liquid tag rof Jekyll sites that allows showing exif data.
 # by: Gosuke Miyashita
+# Modified by: realasking
 #
 # Example usage: {% exif /images/sample.jpg %}
 
 require 'exifr'
+require 'rubygems'
+require 'open-uri'
 
 module Jekyll
   class ExifTag < Liquid::Tag
       def initialize(tag_name, file, token)
       super
-      @image_file = File.expand_path "../" + file.strip , File.dirname(__FILE__)
+      @image_file = ""+ file.strip , File.dirname(__FILE__)
     end
 
     def render(context)
           exif = EXIFR::JPEG::new(@image_file)
           <<-HTML
-    焦点距離 #{exif.focal_length.to_i} mm F#{sprintf "%.1f", exif.f_number.to_f} #{exif.model} ISO #{exif.iso_speed_ratings} #{exif.exposure_time.to_i} 秒露光 #{exif.date_time_original}
+    焦距 #{exif.focal_length.to_i} mm F#{sprintf "%.1f", exif.f_number.to_f} #{exif.model} ISO #{exif.iso_speed_ratings} #{exif.exposure_time.to_i}s 
     HTML
         end
       end
