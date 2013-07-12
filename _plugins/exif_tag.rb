ebf1e30c3c933cbfd4eefdc983629248e0df1e2f
realasking <realasking@mdbbs.org>
2013-07-12 20:49:57 +0800

    测试贴图17

--- _plugins/exif_tag.rb
+++ _plugins/exif_tag.rb
@@ -10,8 +10,11 @@ module Jekyll
   class ExifTag < Liquid::Tag
       def initialize(tag_name, file, token)
       super
-      if file.strip !~ /^https?:\/\//
-      @image_file = File.expand_path "../" + file.strip, File.dirname(__FILE__)
+        if file.strip !~ /^https?:\/\//
+          @image_file = File.expand_path "../" + file.strip, File.dirname(__FILE__)
+        else
+          @image_file = file.strip, File.dirname(__FILE__)
+        end
       end
 
        def render(context)
@@ -20,7 +23,7 @@ module Jekyll
     焦距 #{exif.focal_length.to_i} mm 光圈F#{sprintf "%.1f", exif.f_number.to_f} #{exif.model} ISO #{exif.iso_speed_ratings} 曝光时间#{exif.exposure_time.to_i}s
           HTML
        end
-      end
    end
+end
 
 Liquid::Template.register_tag('exif', Jekyll::ExifTag)
